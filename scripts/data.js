2204bf567161ab7a5aab5ecb1f644d0a43048f0f
Alexey Bobrikov <a.beaverg@gmail.com>
2024-06-25 10:43:04 +0300

    Updated CV and code of CV

---
+++ scripts/data.js
@@ -0,0 +1,13 @@
+// HARD SKILLS:
+document.getElementById("programming_language").innerHTML = "Java";
+document.getElementById("automation").innerHTML = "Maven, Selenium, Appium, Rest-Assured, TestNG, Carina, Selenide, Allure, Jenkins, Log4j, Design Patterns";
+document.getElementById("testing").innerHTML = "Jira, Azure DevOps, Trello, TestRail, TestLink, Qase.io, Test IT, DevTools, Postman, JMeter, Pairwise tool";
+document.getElementById("databases").innerHTML = "SQL (DDL, DML), MySQL Workbench, MyBatis DAO";
+document.getElementById("vcs").innerHTML = "Git (GitHub)";
+document.getElementById("other_skills").innerHTML = "Python, Pytest, Unittest, Selene, Behave, Gherkin, Cypress, HTML, CSS, JavaScript, Markdown, Bootstrap";
+
+// UPDATED:
+document.getElementById("date").innerHTML = "June 25th, 2024";
+
+// COPYRIGHT:
+document.getElementById("copyright").innerHTML = "&#169; Alexey Bobrykov " + new Date().getFullYear();
