b4dabeefce9f36caebfdc051681d833c48683140
Alexey Bobrikov <a.beaverg@gmail.com>
2024-06-26 17:36:02 +0300

    Updated code

--- scripts/data.js
+++ scripts/data.js
@@ -1,13 +1,25 @@
+// POSITION:
+document.getElementById("position").innerHTML = "Java Test Automation Engineer, Manual QA Engineer";
+
 // HARD SKILLS:
 document.getElementById("programming_language").innerHTML = "Java";
 document.getElementById("automation").innerHTML = "Maven, Selenium, Appium, Rest-Assured, TestNG, Carina, Selenide, Allure, Jenkins, Log4j, Design Patterns";
 document.getElementById("testing").innerHTML = "Jira, Azure DevOps, Trello, TestRail, TestLink, Qase.io, Test IT, DevTools, Postman, JMeter, Pairwise tool";
 document.getElementById("databases").innerHTML = "SQL (DDL, DML), MySQL Workbench, MyBatis DAO";
 document.getElementById("vcs").innerHTML = "Git (GitHub)";
-document.getElementById("other_skills").innerHTML = "Python, Pytest, Unittest, Selene, Behave, Gherkin, Cypress, HTML, CSS, JavaScript, Markdown, Bootstrap";
+document.getElementById("other_skills").innerHTML = "Python, Pytest, Unittest, Selene, Behave, Gherkin, Cypress, Youtrack, SOAPUI, HTML, CSS, JavaScript, " + 
+    "Markdown, Bootstrap";
+
+// SOFT SKILLS:
+document.getElementById("soft_skills").innerHTML = "Communication, Attention to detail, Teamwork, Creativity, Adaptability, Work Planning, Constructive feedback, " +
+    "Calmness, Analysis, Logical reasoning, Decision making, Mentorship, Responsibility";
+
+// LANGUAGE:
+document.getElementById("russian").innerHTML = "Native speaker";
+document.getElementById("english").innerHTML = "B1 (Intermediate)";
 
 // UPDATED:
-document.getElementById("date").innerHTML = "June 25th, 2024";
+document.getElementById("date").innerHTML = "June 26th, 2024";
 
 // COPYRIGHT:
 document.getElementById("copyright").innerHTML = "&#169; Alexey Bobrykov " + new Date().getFullYear();
