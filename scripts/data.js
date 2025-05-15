30609be746cbd1a86dfd6713c274a737c6697efb
Alexey Bobrikov <a.beaverg@gmail.com>
2025-05-15 20:24:06 +0300

    updated CV

--- scripts/data.js
+++ scripts/data.js
@@ -1,15 +1,13 @@
 // POSITION:
-document.getElementById("position").innerHTML = "Java Test Automation Engineer, Manual QA Engineer";
+document.getElementById("position").innerHTML = "Java Test Automation Engineer";
 
 // HARD SKILLS:
 document.getElementById("programming_language").innerHTML = "Java";
-document.getElementById("automation").innerHTML = "Maven, Selenium, Appium, RestAssured, TestNG, Zebrunner Carina, Selenide, Playwright, Allure, Logging, Design Patterns";
-document.getElementById("testing").innerHTML = "Jira, Azure DevOps, Trello, TestRail, TestLink, Qase.io, Test IT, DevTools, Postman, JMeter, Pairwise tool";
+document.getElementById("automation").innerHTML = "Maven, Gradle, Selenium, Appium, RestAssured, TestNG, jUnit, Selenide, Playwright, Cucumber, Gherkin, Allure, WireMock";
+document.getElementById("testing").innerHTML = "Jira, Azure DevOps, Trello, TestRail, TestLink, Qase.io, Test IT, DevTools, Postman, Hoppscotch, JMeter, Pairwise tool, UML";
 document.getElementById("ci-cd").innerHTML = "Jenkins, GitHub Actions";
-document.getElementById("databases").innerHTML = "SQL (DDL, DML), MySQL Workbench, MyBatis DAO";
-document.getElementById("vcs").innerHTML = "Git (GitHub)";
-document.getElementById("other_skills").innerHTML = "Python, Pytest, Unittest, Selene, Behave, Gherkin, Cypress, Youtrack, SoapUI, HTML, CSS, JavaScript, " + 
-    "Markdown, Bootstrap";
+document.getElementById("databases").innerHTML = "SQL (DDL, DML), Workbench, DBeaver, OrmLite, HikariCP, JDBC";
+document.getElementById("vcs").innerHTML = "Git";
 
 // SOFT SKILLS:
 document.getElementById("soft_skills").innerHTML = "Communication, Attention to detail, Teamwork, Creativity, Adaptability, Work Planning, Constructive feedback, " +
@@ -20,7 +18,7 @@ document.getElementById("russian").innerHTML = "Native speaker";
 document.getElementById("english").innerHTML = "B1 (Intermediate)";
 
 // UPDATED:
-document.getElementById("date").innerHTML = "July 18th, 2024";
+document.getElementById("date").innerHTML = "May 15th, 2025";
 
 // COPYRIGHT:
 document.getElementById("copyright").innerHTML = "&#169; Alexey Bobrykov " + new Date().getFullYear();
